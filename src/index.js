ff39442afd2071f9e01d50d96860c8cf718eed12
Skarabey DM <mechanoid0415@gmail.com>
2023-01-26 20:50:00 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number)
+{
+  let words = new Array(3)
+
+  const digits = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+  const tens = [, , "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+  const hundred = ~~((number / 100) % 10),
+    ten = ~~((number / 10) % 10),
+    digit = ~~(number % 10)
+
+
+  // Hundreds
+  if (hundred)
+    words[0] = digits[hundred] + " hundred"
+
+  // Tens
+  if (ten)
+    if (ten > 1)
+      words[1] = tens[ten]
+    else if (ten == 1) {
+      words[1] = digits[10 + digit]
+
+      return words.filter(Boolean).join(" ")
+    }
+
+  // Digits
+  if (digit) {
+    if (digit != 0)
+      words[2] = digits[digit]
+  } else if (!number)
+    return "zero"
+
+  return words.filter(Boolean).join(" ")
 }
