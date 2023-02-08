d0768de743c0174efa7583685b5cbc9f5c5222b6
Skarabey DM <mechanoid0415@gmail.com>
2023-02-08 12:52:37 +0300

    feat: solve task again

--- src/index.js
+++ src/index.js
@@ -1,35 +1,27 @@
-module.exports = function toReadable(number)
+function toReadable(number)
 {
-  let words = new Array(3)
+  const digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+  const tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 
-  const digits = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
-  const tens = [, , "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-
-  const hundred = ~~((number / 100) % 10),
-    ten = ~~((number / 10) % 10),
-    digit = ~~(number % 10)
-
-
-  // Hundreds
-  if (hundred)
-    words[0] = digits[hundred] + " hundred"
-
-  // Tens
-  if (ten)
-    if (ten > 1)
-      words[1] = tens[ten]
-    else if (ten == 1) {
-      words[1] = digits[10 + digit]
-
-      return words.filter(Boolean).join(" ")
+  const getWords = (number, mayZero = true) => {
+    // 100 - 999
+    if (number >= 100) {
+      const hundred = Math.trunc(number / 100)
+      number -= hundred * 100
+      return `${digits[hundred - 1]} hundred${number ? (" " + getWords(number, false)) : ""}`
     }
-
-  // Digits
-  if (digit) {
-    if (digit != 0)
-      words[2] = digits[digit]
-  } else if (!number)
-    return "zero"
-
-  return words.filter(Boolean).join(" ")
+    // 20 - 99
+    else if (20 <= number) {
+      const ten = Math.trunc(number / 10)
+      number -= ten * 10
+      return `${tens[ten - 2]}${number ? (" " + getWords(number, false)) : ""}`
+    }
+    // 1 - 19
+    else if (1 <= number) return digits[number - 1]
+    // 0
+    else if (mayZero) return "zero"
+  }
+  return getWords(number)
 }
+
+module.exports = toReadable
